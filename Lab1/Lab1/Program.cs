1c353d0372a13ee63624f23e1bbbe2114560df3c
brownec <cbrowne77@gmail.com>
2014-09-29 19:54:43 +0100

    Add Sphere class

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -62,6 +62,47 @@ namespace cSharpLab1
             Side = side;
         }
     }
+
+    /* Sphere class
+1.	A sphere "is a" ThreeDShape
+5.	Override the ToString() method inherited from ThreeDShape, 
+     * make it display information about the sphere.
+6.	Test the class. Call the method to calculate the volume of a sphere polymorphically. 
+     * Display details of the sphere object.
+     * Make a collection of spheres and call various methods on each sphere
+ */
+    
+    public class Sphere : Shape
+    {
+        /* 2.	The class should have field to store the radius of the sphere, 
+                and a read-write property corresponding to the field. 
+                The radius must be always positive. */
+        public double Radius
+        {
+            get;
+            set;
+        }
+
+        /* 4.	Override the method to calculate the volume 
+         * i.e. the volume of a sphere is PI * radius * radius * radius. */
+        public override double Area
+        {
+            get
+            {
+                double PI = 3.14;
+                return PI * Radius * Radius * Radius;
+            }
+        }
+
+        // 3.	Implement appropriate constructors for the class.
+        // Default Constructor
+        public Sphere(String id, double radius)
+            :base(id)
+        {
+            Radius = radius;
+        }
+    }
+
     public class Test
     {
         static void Main()
@@ -71,6 +112,11 @@ namespace cSharpLab1
             Console.WriteLine();
             Console.WriteLine("The Shape Id is: " + s.Id);
             Console.WriteLine("The volume of the area of " + s.Id + " is " + s.Area);
+            Console.ReadLine();
+
+            Sphere sp = new Sphere("Sphere 1", 10);
+            Console.WriteLine("The Shape Id is: " + sp.Id);
+            Console.WriteLine("The volume of the area of " + sp.Id + " is " + sp.Area);
             Console.ReadLine();
         }
     }
